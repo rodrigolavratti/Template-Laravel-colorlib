e9d7ee98ec4ed0c773737f7eb70b08fc024bcabd
rodrigolavratti <rodrigo.l.lavratti@gmail.com>
2020-04-04 16:58:30 -0300

    Criação de rotas para os sub-links

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,5 @@ use Illuminate\Support\Facades\Route;
 });*/
 
 Route::get('/', 'HomeController@index')->name('home');
+Route::get('/ui-buttons', 'TesteController@index')->name('ui-buttons.index');
 
-/*Route::get('/', function() {
-    return view('home');
-})->name('home');*/
